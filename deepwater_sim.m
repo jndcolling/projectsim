bd051c81ea3d0d0e4d47b125d57930a337d8efc4
John <John@TABLET-1HN9FFS7>
2023-05-16 14:14:46 +0100

    defining variables

--- deepwater_sim.m
+++ deepwater_sim.m
@@ -1,5 +1,18 @@
- clc
+clc
 clear
 close all
 
-test
+xmin = 0;
+xmax = 20;
+nx = 100; %number of datapoints
+
+ymin = 0;
+ymax = 20;
+ny = nx; %making it a square makes some off the plotting later easier
+
+dx = (xmax - xmin)/(nx-1);
+dy = (ymax - ymin)/(ny-1);
+
+x = xmin:dx:xmax;
+y = ymin:dy:ymax;
+
