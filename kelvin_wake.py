98035bee4f586097dbf79aadf039c9483b9aa5e9
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-21 18:41:45 +0100

    Attempting to plot the Kelvin wake by integrating a function described by the Darmon, Benzaquen, Raphaël paper. Using a (strange) method that involves looping through every x and y and integrating there. Output is unexpected.

--- kelvin_wake.py
+++ kelvin_wake.py
@@ -1,20 +1,34 @@
 import numpy as np
 import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 x_list = np.linspace(0, 10, 10)
 y_list = np.linspace(0, 10, 10)
-z_list = []
+z_list = np.zeros(100)
+
+
 l = 0.5   # wavelength
 K = 2 * np.pi / l
 P = np.exp(-K**2 / (4 * np.pi ** 2))
 Fr = 0.5
 
+count = 0
 for x in x_list:
     for y in y_list:
         def integrand(theta):
-            return (1j * np.pi * np.exp(-1j*(np.cos(theta)*x - np.sin(theta)*y) / ((Fr * np.cos(theta))**2)))\
+            return (1j * P * np.pi * np.exp(-1j*(np.cos(theta)*x - np.sin(theta)*y) / ((Fr * np.cos(theta))**2)))\
                    / ((Fr * np.cos(theta))**4)
         z = integrate.quad(integrand, np.pi / -2, np.pi / 2)
-        z_list += z
-        print(z_list)
-print(z_list)
+        np.put(z_list, count, z)
+        count += 1
+X, Y = np.meshgrid(x_list, y_list)
+z_list = np.reshape(z_list, (10, 10))
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(X, Y, z_list, vmin=z_list.min() * 2, cmap=cm.Blues)
+
+ax.set(xticklabels=[],
+       yticklabels=[],
+       zticklabels=[])
+
+plt.show()
