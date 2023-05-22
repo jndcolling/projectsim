3a4232f18f7fd44fac9bc5572db7fe5487ad0bf1
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-22 11:06:06 +0100

    Top-down view, but still unreliable. Sometimes gives something close to the expected result.

--- kelvin_wake.py
+++ kelvin_wake.py
@@ -3,15 +3,16 @@ import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-x_list = np.linspace(-10, 10, 20)
-y_list = np.linspace(-10, 10, 20)
-z_list = np.zeros(400)
+n_points = 30
+x_list = np.linspace(-10, 10, n_points)
+y_list = np.linspace(-10, 10, n_points)
+z_list = np.zeros(n_points**2)
 
 
 l = 0.5   # wavelength
 K = 2 * np.pi / l
 P = np.exp(-K**2 / (4 * np.pi ** 2))
-Fr = 0.5
+Fr = 0.8
 
 count = 0
 for x in x_list:
@@ -23,8 +24,9 @@ for x in x_list:
         np.put(z_list, count, z)
         count += 1
 X, Y = np.meshgrid(x_list, y_list)
-z_list = np.reshape(z_list, (20, 20))
+z_list = np.reshape(z_list, (n_points, n_points))
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.view_init(elev=90, azim=0)
 ax.plot_surface(X, Y, z_list, vmin=z_list.min() * 2, cmap=cm.Blues)
 
 ax.set(xticklabels=[],
