cd7c0bfd31cb3d14d874c6c81d8a8ae23d17615c
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-25 15:01:45 +0100

    Now adds many different wavelengths

--- wavefronts_3D.m
+++ wavefronts_3D.m
@@ -3,12 +3,12 @@ xlim([-10 10])    % limits of axes
 ylim([-10 10])
 
 %/ define coordinates /%
-xmin = -10;
-xmax = 10;
+xmin = -20;
+xmax = 2;
 nx = 100;
 
-ymin = -10;
-ymax = 10;
+ymin = -20;
+ymax = 20;
 ny = nx;
 
 dx = (xmax - xmin)/(nx-1);
@@ -19,17 +19,21 @@ y = ymin:dy:ymax;
 
 [X, Y] = meshgrid(x, y);
 
-%/ define Z /%
-r = sqrt(X.^2 + Y.^2);    % radius
-l = 1;    % wavelength
-k = 2 * pi / l;    % wave number
-Z = sin(k*r);
-surf(X,Y,Z)
-v = 1;    % speed of the boat
-
-for t = 1:10
-    r = sqrt((X-v*t).^2+Y.^2);
-    Z = Z + sin(k*r);
-    pause(1)    % wait 1 second before next iteration
+for p = 1:10
+    l = p / 5;
+    %/ define Z /%
+    r = sqrt(X.^2 + Y.^2);    % radius
+    %l = 1;    % wavelength
+    k = 2 * pi / l;    % wave number
+    Z = sin(k*r);
     surf(X,Y,Z)
+    v = 1;    % speed of the boat
+    c = sqrt(9.81 * l / (2 * pi));    % speed of the wave
+    
+    for t = 1:10
+        r = sqrt((X-v*t).^2+Y.^2);
+        Z = Z + sin(k*r);
+        %pause(1)    % wait 1 second before next iteration
+        surf(X,Y,Z)
+    end
 end
