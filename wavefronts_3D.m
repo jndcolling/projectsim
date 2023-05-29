7df291d0e4c6b5aa7b2b23fe994260636e8f2239
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-29 22:06:56 +0100

    Jason pointed out a major error that caused me to initialise Z inside the for loop. It is now initialised outside. A wake can sometimes be made out with the right conditions. Further investigation required.

--- wavefronts_3D.m
+++ wavefronts_3D.m
@@ -1,5 +1,5 @@
 %/ define coordinates /%
-xmin = -60;    % adjust bounds as desired
+xmin = 0;    % adjust bounds as desired
 xmax = 60;
 nx = 300;
 ymin = -60;
@@ -13,26 +13,25 @@ y = ymin:dy:ymax;
 [X, Y] = meshgrid(x, y);
 
 %/ determine height Z /%
-
+Z = 0;      % initialise height Z
 g = 9.81;
-for p = 1:200        % looping 200 times to get 200 different frequencies
-    w = p / 20;      % define each frequency w
+for p = 1:500        % looping 200 times to get 200 different frequencies
+    w = p / 30;      % define each frequency w
     k = w^2 / g;     % wavenumber, found from dispersion relation
     cp = sqrt(g/k);  % phase velocity of wave
     cg = 0.5 * cp;   % group velocity of wave
-    v = 1.5;    % speed of the boat
-    Z = 0;      % initialise height Z
-    tmax = 50;  % number of seconds to loop t over
+    v = 1.9;    % speed of the boat
+    tmax = 40;  % number of seconds to loop t over
     tdivide = 20; % just to get smaller intervals
     for t = 0:tmax    % loop for 50 time intervals
         max_distance = cg * (tmax - t);
         tprime = t/tdivide;    % divide t to get smaller intervals
-        r = sqrt((X+v*tprime).^2+Y.^2);    % distance from boat's position
+        r = sqrt((X-v*tprime).^2+Y.^2);    % distance from boat's position
         if r > max_distance    % if we exceed the max distance
             r = 0;             % set r and w to 0, so the contribution to Z
             w = 0;             % is also 0
         end
-        Z = Z + sin(k*r - w*(tprime-tmax/tdivide));    % add new Z part
+        Z = Z + sin(k*r - w*(tmax/tdivide - tprime));    % add new Z part
     end
 end
 surf(X,Y,Z)    % plot
