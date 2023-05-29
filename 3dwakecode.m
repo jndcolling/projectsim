f0ce7f2eb11cc77b2caa226465b2a2f155a4c55c
3890plkh <134627099+3890plkh@users.noreply.github.com>
2023-05-29 20:34:20 +0100

    Update 3dwakecode.m
    added comments

--- 3dwakecode.m
+++ 3dwakecode.m
@@ -19,17 +19,17 @@ n=101; % this is number of time intervals we are looking at
 finaltime=10; % this is "how long" the simulation runs for. At finaltime=0, boat is at origin.  
 times=linspace(0,finaltime,n); % now we will look at the wave released at each t=times  
 boatspeed=20;
-for i=1:21%looping over certain wavelengths - only one wavelength for now
+for i=1:21 % looping over certain wavelengths 
     k=i; % this is the actual value for wavelength we will be using
     omega=sqrt(9.81.*k);
-    phasevel=sqrt(9.81./k); %
+    phasevel=sqrt(9.81./k); %using deep water dispersion relations
     groupvel=0.5*phasevel;
     for j=1:n
-        t=times(j);
-        r=sqrt((X-boatspeed*t).^2+(Y).^2); %distance from boat position at time t
-        maxdistance=groupvel*(finaltime-t);
-        z=z+cos(k.*r-omega*(finaltime-t));
-        if r>=maxdistance %delete the contributions that don't physically make sense
+        t=times(j); % At time t, we will now attenpt to add contribution from disturbance created at boat position
+        r=sqrt((X-boatspeed*t).^2+(Y).^2); % distance from boat position at time t
+        maxdistance=groupvel*(finaltime-t); % this is the furthest the wave could have travelled 
+        z=z+cos(k.*r-omega*(finaltime-t)); % add contribution for the pattern at final time
+        if r>=maxdistance % delete the contributions that don't physically make sense
             z=z-cos(k.*r-omega*(finaltime-t));
         end
     end
