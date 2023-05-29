aa4c16ffe8c6fe22eb3685d04b93a1ce38b367d1
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-29 21:26:58 +0100

    Based on Jason's latest commit, impose a limit on the range of waves. The wake can only be generated if the waves can feasbily reach that point. Resembles a wake, but the amplitude is absurdly high.

--- wavefronts_3D.m
+++ wavefronts_3D.m
@@ -1,9 +1,9 @@
 %/ define coordinates /%
-xmin = -40;    % adjust bounds as desired
-xmax = 40;
+xmin = -60;    % adjust bounds as desired
+xmax = 60;
 nx = 300;
-ymin = 10;
-ymax = 50;
+ymin = -60;
+ymax = 60;
 ny = nx;
 
 dx = (xmax - xmin)/(nx-1);
@@ -14,15 +14,25 @@ y = ymin:dy:ymax;
 
 %/ determine height Z /%
 
-for p = 1:200      % looping 200 times to get 200 different frequencies
-    w = p / 20;    % define each frequency w
-    k = w^2 / 9.81;    % wavenumber, found from dispersion relation
+g = 9.81;
+for p = 1:200        % looping 200 times to get 200 different frequencies
+    w = p / 20;      % define each frequency w
+    k = w^2 / g;     % wavenumber, found from dispersion relation
+    cp = sqrt(g/k);  % phase velocity of wave
+    cg = 0.5 * cp;   % group velocity of wave
     v = 1.5;    % speed of the boat
     Z = 0;      % initialise height Z
-    for t = 0:50    % loop for 50 time intervals
-        tprime = t/20;    % divide t to get smaller intervals
+    tmax = 50;  % number of seconds to loop t over
+    tdivide = 20; % just to get smaller intervals
+    for t = 0:tmax    % loop for 50 time intervals
+        max_distance = cg * (tmax - t);
+        tprime = t/tdivide;    % divide t to get smaller intervals
         r = sqrt((X+v*tprime).^2+Y.^2);    % distance from boat's position
-        Z = Z + sin(k*r);    % add new wave to the existing Z
+        if r > max_distance    % if we exceed the max distance
+            r = 0;             % set r and w to 0, so the contribution to Z
+            w = 0;             % is also 0
+        end
+        Z = Z + sin(k*r - w*(tprime-tmax/tdivide));    % add new Z part
     end
 end
 surf(X,Y,Z)    % plot
