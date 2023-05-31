171b90cc249a60fedf6fad388e06db0c71c7fafe
adequateusername <christopherjao2021@student.cis.edu.hk>
2023-05-31 15:31:30 +0100

    Considering the power spectrum to be Gaussian. Still not convincing

--- wavefronts_3D.m
+++ wavefronts_3D.m
@@ -1,9 +1,9 @@
 %/ define coordinates /%
 xmin = 0;    % adjust bounds as desired
-xmax = 200;
-nx = 300;
-ymin = -200;
-ymax = 200;
+xmax = 90;
+nx = 200;
+ymin = -100;
+ymax = 100;
 ny = nx;
 
 dx = (xmax - xmin)/(nx-1);
@@ -15,14 +15,15 @@ y = ymin:dy:ymax;
 %/ determine height Z /%
 Z = 0;      % initialise height Z
 g = 9.81;
-for p = 1:200        % looping 200 times to get 200 different frequencies
+v = 1.5;    % speed of the boat
+tmax = 300;  % number of time periods to loop t over (not seconds)
+tdivide = 5; % just to get smaller intervals
+for p = 1:300        % looping 200 times to get 200 different frequencies
     w = p / 3;      % define each frequency w
     k = w^2 / g;     % wavenumber, found from dispersion relation
     cp = sqrt(g/k);  % phase velocity of wave
     cg = 0.5 * cp;   % group velocity of wave
-    v = 1.8;    % speed of the boat
-    tmax = 60;  % number of seconds to loop t over
-    tdivide = 30; % just to get smaller intervals
+    A = exp(-k*v);    % amplitude of the wave produced
     for t = 0:tmax    % loop for 50 time intervals
         max_distance = cg * (tmax - t);
         tprime = t/tdivide;    % divide t to get smaller intervals
@@ -31,8 +32,8 @@ for p = 1:200        % looping 200 times to get 200 different frequencies
             r = 0;             % set r and w to 0, so the contribution to Z
             w = 0;             % is also 0
         end
-        Z = Z + sin(k*r - w*(tmax/tdivide - tprime));    % add new Z part
+        Z = Z + A * sin(k*r - w*(tmax/tdivide - tprime));  % add new Z part
     end
 end
 surf(X,Y,Z)    % plot
-zlim([10 200])    % limit range of z values
+%zlim([10 200])    % limit range of z values
