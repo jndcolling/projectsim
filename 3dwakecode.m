828b7769463d6385e35147f3a685f6d96ea5e384
3890plkh <134627099+3890plkh@users.noreply.github.com>
2023-06-08 11:28:54 +0100

    Update 3dwakecode.m
    We now iterate to create k values from which a frequency is calculated rather than the other way around. This seems to solve some of the issues that we had, but the shallow water case is still being weird. There is also code for 4 moving point sources that I am using to model a box. The contributions from the "middle" of the box is unphysical.

--- 3dwakecode.m
+++ 3dwakecode.m
@@ -1,10 +1,10 @@
 %/ define coordinates /%
 xmin = 0;    % adjust bounds as desired
-xmax = 300;
+xmax = 250;
 nx = 500;
 ymin = -100;
 ymax = 100;
-ny = nx;
+ny = 200;
 
 dx = (xmax - xmin)/(nx-1);
 dy = (ymax - ymin)/(ny-1);
@@ -13,21 +13,43 @@ y = ymin:dy:ymax;
 [X, Y] = meshgrid(x, y);
 
 %/ determine height Z /%
+h = 0.1;
 Z = 0;      % initialise height Z
 g = 9.81;
-v = 5;    % speed of the boat
-tmax = 50;  % number of time periods to loop t over (not seconds)
-for p = 1:200        % looping 200 times to get 200 different frequencies
-    w = p/20;      % define each frequency w
-    k = w^2 / g;     % wavenumber, found from dispersion relation
-    cp = sqrt(g/k);  % phase velocity of wave
-    cg =  cp;   % group velocity of wave
+v = 6;    % speed of the boat
+tmax = 35;  % final time
+for p = 30:3000   % set up 10000 iterations
+    k = p/50 ;      % define each wavenumber 
+
+    %  general dispersion relation
+%    w = sqrt(g*k*tanh(k*h));     % frequency, found from general dispersion relation
+%    cp = sqrt(g/k.*tanh(k.*h));  % phase velocity of wave
+%    cg =  0.5*cp.*(1+(h*k)./(cosh(k*h).*sinh(k*h)));   % group velocity of wave
+   
+    % deep water relation
+    w = sqrt(g*k); 
+    cp= sqrt(g/k);
+    cg= 0.5*cp;
+
     A = sqrt(exp(-k*v));    % amplitude of the wave produced
-    for t = 0:tmax    % loop for 50 time intervals
-        max_distance = cg * (tmax - t);
-        r = sqrt((X-v*t).^2+Y.^2);    % distance from boat's position
-        Z = Z + A * sin(k*r.*(r<max_distance) - w*(tmax - t).*(r<max_distance));  % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
+    for i = 0:tmax    % loop from initial itme to final time
+        t = i;
+        max_distance = cg * (tmax - t); % max distance a wave can travel
+        
+        % Single point source code
+%       r = sqrt((X-v*t).^2+(Y).^2); % distance from boat position 
+%       Z = Z - A*sin(k*r.*(r<max_distance) - w*(tmax - t).*(r<max_distance));    % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
+        % btw. the negative is me cheating. the surface was upside down for some reason
+
+        % 4 moving point sources
+        r1 = sqrt((X-v*t).^2+(Y+4).^2);    % distance from boat's position
+        r2 = sqrt((X-v*t).^2+(Y-4).^2);
+        r3 = sqrt((X-v*t+10).^2+(Y+4).^2);    % distance from boat's position
+        r4 = sqrt((X-v*t+10).^2+(Y-4).^2);
+        Z = Z - A*sin(k*r1.*(r1<max_distance) - w*(tmax - t).*(r1<max_distance));  % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
+        Z = Z - A*sin(k*r2.*(r2<max_distance) - w*(tmax - t).*(r2<max_distance));
+        Z = Z - A*sin(k*r3.*(r3<max_distance) - w*(tmax - t).*(r3<max_distance));  % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
+        Z = Z - A*sin(k*r4.*(r4<max_distance) - w*(tmax - t).*(r4<max_distance));
     end
 end
-pcolor(X,Y,Z)    % plot
-%zlim([-50 30])    % limit range of z values
+surf(X,Y,Z)    % plot
