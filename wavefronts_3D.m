72a09f1294a4e970d5d5f3c35e58e7b156608875
/foodiebonus <54303903+AdequateUsername@users.noreply.github.com>
2023-06-14 21:07:15 +0100

    The one that works

--- wavefronts_3D.m
+++ wavefronts_3D.m
@@ -1,10 +1,10 @@
 %/ define coordinates /%
 xmin = 0;    % adjust bounds as desired
-xmax = 17;
+xmax = 12;
 nx = 600;
-ymin = -10;
-ymax = 10;
-ny = 150;
+ymin = -6;
+ymax = 6;
+ny = 200;
 
 dx = (xmax - xmin)/(nx-1);
 dy = (ymax - ymin)/(ny-1);
@@ -13,51 +13,47 @@ y = ymin:dy:ymax;
 [X, Y] = meshgrid(x, y);
 
 %/ determine height Z /%
-h = 20;                % water depth
+h = 2;                % water depth
 Z = 0;                  % initialise height Z
 g = 9.81;               % gravitational acceleration
-v = 0.5;                  % speed of the boat
-l = 0.25/(9.81 * 0.5);     % length of the boat
-Fr = v / sqrt(g*l);     % Froude number
+v = 1.56604598;                  % speed of the boat
+Fr = 0.5;                 % Froude number
+l = v^2/(9.81 * Fr^2);     % length of the boat
 sigma = 0.072;          % surface tension https://www.engineeringtoolbox.com/water-surface-tension-d_597.html
 rho = 1000;             % density of water
 k_0 = 2 * pi / l;
-w_0 = sqrt(g*k_0);                     % for deep water
+%w_0 = sqrt(g*k_0);                     % for deep water
 %w_0 = sqrt(g*k_0*tanh(k_0*h));          % for shallow water
-%w_0 = sqrt((k_0*g + sigma*k_0^3/rho) * tanh(k_0*h));  % for surface tension
-k_divider = 20;         % to get smaller increments of k
-dt = 0.05;              % time interval
-tmax = 300;              % final time to iterate to
-for k = k_0/6:k_0*6     % set up many iterations for k
-    %k = p/k_divider ;      % define each wavenumber using the divider
+w_0 = sqrt((k_0*g + sigma*k_0^3/rho) * tanh(k_0*h));  % for surface tension
+dt = 0.03;              % distance interval (this is NOT a time)
+tmax = 400;              % final distance to iterate to
+for k = k_0/5:k_0*5     % set up many iterations for k centred on k_0
 
-    %/ general dispersion relation (surface tension and depth considered)
-    %w = sqrt((k*g + sigma*k^3/rho) * tanh(k*h));
-    %cp = w / k;
-    %cg = ((g + 3*sigma*k^2/rho) * tanh(k*h) + (k*g + sigma*k^3/rho) / (cosh(k*h))^2) / (2*w);
+    %/ general relation (surface tension and depth considered)
+    w = sqrt((k*g + sigma*k^3/rho) * tanh(k*h));
+    cp = w / k;
+    cg = ((g + 3*sigma*k^2/rho) * tanh(k*h) + (k*g + sigma*k^3/rho) / (cosh(k*h))^2) / (2*w);
 
-    %/  shallow-depth dispersion relation
+    %/  shallow water relation
     %w = sqrt(g*k*tanh(k*h));     % frequency, found from general dispersion relation
     %cp = sqrt(g/k.*tanh(k.*h));  % phase velocity of wave
     %cg =  0.5*cp.*(1+(h*k)./(cosh(k*h).*sinh(k*h)));   % group velocity of wave
    
     %/ deep water relation
-    w = sqrt(g*k); 
-    cp= sqrt(g/k);
-    cg= 0.5*cp;
+    %w = sqrt(g*k); 
+    %cp= sqrt(g/k);
+    %cg= 0.5*cp;
     
     % amplitude of the wave produced - assumed to be gaussian distributed
-    %A = sqrt(exp(-w^2*v/g));    %/ in the article it uses this gaussian specifically
-    A = exp(-v*(w-w_0)^2/g);    %/ adjusted to be centred on w_0 – defined for the wavelength equal to boat length
+    A = sqrt(exp(-v*(w-w_0)^2/g));    % adjusted to be centred on w_0 – defined for the wavelength equal to boat length
 
-    for i = 0:tmax                      % loop from initial time to final time
-        t = i * dt;
-        max_distance = cg * (tmax*dt - t);    % max distance a wave can travel
+    for i = 0:tmax                      % loop over set spacings i apart, up to tmax
+        t = i * dt;                     % scale i by dt to get intervals of dt
+        max_distance = cg * (tmax*dt - t)/v;    % max distance a wave can travel
         
         %/ For a single point boat
         r = sqrt((X-t).^2+(Y).^2);       % distance from boat position 
-        Z = Z - A*sin(k*r.*(r<max_distance) - w*(tmax*dt - t)/(v*dt).*(r<max_distance));    % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
-        % may sometimes need to subtract from Z. Unclear why.
+        Z = Z + A*sin(k*r.*(r<max_distance) - w*(tmax*dt-t)/v.*(r<max_distance));    % add new Z part if feasible https://uk.mathworks.com/matlabcentral/answers/474717-mesh-surf-plot-of-function-with-if-statements
 
         %/ For a boat consisting of four moving points
 %        r1 = sqrt((X-v*t).^2+(Y+4).^2);    % distance from boat's position
@@ -71,6 +67,8 @@ for k = k_0/6:k_0*6     % set up many iterations for k
     end
 end
 surf(X,Y,Z)    % plot
-disp(Fr)
+disp("v = " + v)
+disp("l = " + l)
+disp("Fr = " + Fr)
 alpha = atand((sqrt(2*pi*Fr^2 - 1)) / (4*pi*Fr^2 - 1));
-disp(alpha)
+disp("α_exp = " + alpha)
